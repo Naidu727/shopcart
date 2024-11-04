c0f0d2ec0be0e73d31fa6662122cd9249b8630b3
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-11-05 01:25:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000
 dotenv.config();
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors({
+  origin: 'https://shopcart-leak.onrender.com'
+}));
 
 mongoose
     .connect(process.env.MONGO_URL, {
